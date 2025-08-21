397c667e7fd387f2a34d2931d1c3ab0d10f7d7da
Eduardo Canos <eduardo.p.r.canos@gmail.com>
2025-08-21 18:10:20 -0300

    Funçao de calcular o imposto de renda de um salario

---
+++ simuladorir/android/app/src/main/kotlin/com/example/simuladorir/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.simuladorir
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ simuladorir/lib/main.dart
@@ -0,0 +1,158 @@
+//pacote que contém os componentes do layout do formulário
+import 'package:flutter/material.dart';
+ 
+//método principal da classe
+void main() => runApp(
+      //MaterialApp->utiliza as partes gráficas
+      //para elaboração dos layouts
+      MaterialApp(
+        //primeiramente executa a classe Home
+        home: Home(),
+        //utiliza um pequeno banner no aplicativo
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+ 
+//início
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+ 
+class HomeState extends State<Home> {
+  //para poder utilizar as ações dos botões de acordo
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+ 
+  //criando os campos de entrada de dados
+  TextEditingController _salarioController = TextEditingController();
+  
+ 
+  //contém valor do resultado final
+  String _result = "";
+ 
+  //método para zerar todos os campos quando o aplicativo
+  //for aberto
+  @override
+  void initState() {
+    //iniciando o estado do aplicativo ao ser executado
+    super.initState();
+    //chamando o método para limpar os dados informados
+    limpaCampos();
+  }
+ 
+  void limpaCampos() {
+    _salarioController.text = '';
+    
+ 
+    setState(() {
+      _result = "";
+    });
+  }
+ 
+  void calcularIMC() {
+    double slr = double.parse((_salarioController.text).replaceAll(',', '.'));
+    double result;
+    double imposto;
+    {
+      if (slr <= 2000.00) {
+        result = slr;
+      } else if (slr >= 2001.00 && slr < 4000.00) {
+        imposto = slr * 0.10;
+        result = slr - imposto;
+      } else if (slr >= 4001.00 && slr < 6000.00) {
+        imposto = slr * 0.15;
+        result = slr - imposto;
+      }else {
+        imposto = slr * 0.20;
+        result = slr - imposto;
+      
+  }
+ 
+    setState(() {
+    _result = ("O valor do seu salario é ${(result.toStringAsFixed(2))}");
+    });
+  }
+  }
+ 
+  //criando o botão para calcular o Valor a Pagar
+  Widget buildCalcularButton() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 36.0),
+      child: ElevatedButton(
+        onPressed: () {
+          if (_formKey.currentState!.validate()) {
+            calcularIMC();
+          }
+        },
+        child: Text('Calcule o salario', style: TextStyle(color: Colors.red)),
+      ),
+    );
+  }
+ 
+  //método para configurar o resultado em uma Text
+  Widget buildTextResult() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 36.0),
+      child: Text(
+        _result,
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: buildAppBar(),
+        backgroundColor: Colors.orange[100],
+        body: SingleChildScrollView(
+            padding: EdgeInsets.all(20.0), child: buildForm()));
+  }
+ 
+  AppBar buildAppBar() {
+    return AppBar(
+      title: Text('Calcule o salario'),
+      backgroundColor: Colors.orange,
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            limpaCampos();
+          },
+        )
+      ],
+    );
+  }
+ 
+  Form buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          buildTextFormField(
+              label: "Salario: ",
+              error: "Insira o salario",
+              controller: _salarioController),
+          buildTextResult(),
+          buildCalcularButton(),
+        ],
+      ),
+    );
+  }
+ 
+  //formatar e exibir msg de erro nos inputs (entrada
+  //de dados)
+  TextFormField buildTextFormField(
+      {required TextEditingController controller, required String error, required String label}) {
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(labelText: label),
+      controller: controller,
+      validator: (text) {
+        //verifica se o valor foi digitado
+        return text!.isEmpty ? error : null;
+      },
+    );
+  }
+}
